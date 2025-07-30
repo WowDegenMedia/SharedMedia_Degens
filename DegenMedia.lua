fa3782b35034da3be68289c53c47d83f77e1c3e9
Tas <darwand.1997@gmail.com>
2025-07-31 00:48:23 +0200

    Initial commit

---
+++ DegenMedia.lua
@@ -0,0 +1,69 @@
+-- tab size is 4
+-- registrations for media from the client itself belongs in LibSharedMedia-3.0
+
+local LSM = LibStub("LibSharedMedia-3.0")
+local koKR, ruRU, zhCN, zhTW, western = LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_ruRU, LSM.LOCALE_BIT_zhCN, LSM.LOCALE_BIT_zhTW, LSM.LOCALE_BIT_western
+
+local MediaType_BACKGROUND = LSM.MediaType.BACKGROUND
+local MediaType_BORDER = LSM.MediaType.BORDER
+local MediaType_FONT = LSM.MediaType.FONT
+local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+LSM:Register(MediaType_FONT, "Megabomb", [[Interface\Addons\SharedMedia_Degens\font\Megabomb.ttf]])
+LSM:Register(MediaType_FONT, "Megabomb Italic", [[Interface\Addons\SharedMedia_Degens\font\MegabombItalic.ttf]])
+LSM:Register(MediaType_FONT, "Moonglade", [[Interface\Addons\SharedMedia_Degens\font\MoongladeDemoBold.ttf]])
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cffff4ff0Momo - Eat my ass|r", [[Interface\Addons\SharedMedia_Degens\sound\momo2_eat_my_ass.ogg]])
+
+LSM:Register("sound", "|cffff4ff0Degens - Intro|r", [[Interface\Addons\SharedMedia_Degens\sound\degens_intro.ogg]])
+LSM:Register("sound", "|cffff4ff0Degens - Moan|r", [[Interface\Addons\SharedMedia_Degens\sound\degens_moan.ogg]])
+LSM:Register("sound", "|cffff4ff0Degens - HORSE CONCH|r", [[Interface\Addons\SharedMedia_Degens\sound\degens_horseConchFull.ogg]])
+LSM:Register("sound", "|cffff4ff0Degens - Horse Conch|r", [[Interface\Addons\SharedMedia_Degens\sound\degens_horseConchShort.ogg]])
+LSM:Register("sound", "|cffff4ff0Degens - Huh|r", [[Interface\Addons\SharedMedia_Degens\sound\degens_huh.ogg]])
+
+LSM:Register("sound", "|cffff4ff0Sinder - PPs|r", [[Interface\Addons\SharedMedia_Degens\sound\sinder_pps.ogg]])
+LSM:Register("sound", "|cffff4ff0Sinder - No no|r", [[Interface\Addons\SharedMedia_Degens\sound\sinder_no_no.ogg]])
+
+LSM:Register("sound", "|cffff4ff0Filian - Shylily's titties|r", [[Interface\Addons\SharedMedia_Degens\sound\orca_milkers.ogg]])
+
+LSM:Register("sound", "|cffff4ff0Shylily - Yippee|r", [[Interface\Addons\SharedMedia_Degens\sound\shylily_yippee.ogg]])
+LSM:Register("sound", "|cffff4ff0Shylily - Cock|r", [[Interface\Addons\SharedMedia_Degens\sound\Shylily-cock.ogg]])
+
+LSM:Register("sound", "|cffff4ff0Bao - Plap Plap Plap|r", [[Interface\Addons\SharedMedia_Degens\sound\bao_plap_plap_plap.ogg]])
+LSM:Register("sound", "|cffff4ff0Bao - Read|r", [[Interface\Addons\SharedMedia_Degens\sound\bao_read.ogg]])
+LSM:Register("sound", "|cffff4ff0Bao - Meow|r", [[Interface\Addons\SharedMedia_Degens\sound\bao_meow.ogg]])
+
+LSM:Register("sound", "|cffff4ff0Numi - Hold|r", [[Interface\Addons\SharedMedia_Degens\sound\numi_hold.ogg]])
+LSM:Register("sound", "|cffff4ff0Nerissa - Good Girl|r", [[Interface\Addons\SharedMedia_Degens\sound\nerissa_good_girl.ogg]])
+
+LSM:Register("sound", "|cffff4ff0Eiya - Goo Goo Muck|r", [[Interface\Addons\SharedMedia_Degens\sound\eiya_gogomuck.ogg]])
+LSM:Register("sound", "|cffff4ff0Eiya - WoW Gamer|r", [[Interface\Addons\SharedMedia_Degens\sound\eiya_wow_gamer.ogg]])
+LSM:Register("sound", "|cffff4ff0Eiya - Thats Cwinge|r", [[Interface\Addons\SharedMedia_Degens\sound\eiya_thats_cwinge.ogg]])
+LSM:Register("sound", "|cffff4ff0Eiya - Miffic Pwus UwU|r", [[Interface\Addons\SharedMedia_Degens\sound\eiya_miffic_pwus_uwu.ogg]])
+
+LSM:Register("sound", "|cffff4ff0Randal - Moist|r", [[Interface\Addons\SharedMedia_Degens\sound\randal_moist.ogg]])
+LSM:Register("sound", "|cffff4ff0?? - I eat cock|r", [[Interface\Addons\SharedMedia_Degens\sound\i_eat_cock.ogg]])
+
+LSM:Register("sound", "|cffff4ff0?? - Body shape|r", [[Interface\Addons\SharedMedia_Degens\sound\body_shape.ogg]])
+
+LSM:Register("sound", "|cffff4ff0Kanye - Nigga Heil Hitler|r", [[Interface\Addons\SharedMedia_Degens\sound\kanye_nigga_heil_hitler.ogg]])
+LSM:Register("sound", "|cffff4ff0Niggas in Miami|r", [[Interface\Addons\SharedMedia_Degens\sound\niggas_in_miami.ogg]])
+LSM:Register("sound", "|cffff4ff0Donald duck - Heil hitler|r", [[Interface\Addons\SharedMedia_Degens\sound\heil_duck.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
